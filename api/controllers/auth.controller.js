45ddb006fe6f90f62a7cb0544991d0f9318b22d3
Mohammed Musharraf <musharraf.personal@gmail.com>
2024-01-16 23:56:36 +0530

    complete signin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -20,15 +20,19 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, "User Not Found"));
+    if (!validUser) {
+      throw errorHandler(404, "User Not Found");
+    }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "Wrong Crendentials!"));
+    if (!validPassword) {
+      throw errorHandler(401, "Wrong Credentials!");
+    }
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    const { password:pass, ...rest } = validUser._doc;
+    const { password: pass, ...rest } = validUser._doc;
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json(validUser);
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to="/signin">
+          <Link to="/sign-in">
             <li className=" text-slate-700 hover:underline">Sign in</li>
           </Link>
         </ul>
