d6f8e175d3590ade97cd86933c21d7fe11f8af15
Mohammed Musharraf <musharraf.personal@gmail.com>
2024-02-04 20:29:28 +0530

    complete update listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import RoutePrivate from "./components/RoutePrivate";
 import CreateListing from "./pages/CreateListing";
+import EditListing from "./pages/EditListing";
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Route element={<RoutePrivate />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/update-listing/:listingId" element={<EditListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -276,7 +276,9 @@ export default function Profile() {
 
               <div className="flex flex-col item-center">
                 <button className="text-red-700 uppercase"  onClick={() => handleListingDelete(listing._id)}>Delete</button>
-                <button className="text-green-700 uppercase">Edit</button>
+                  <Link to={`/update-listing/${listing._id}`}>
+                <button className="text-green-700 uppercase" >Edit</button>
+                </Link>
               </div>
             </div>
           ))}
