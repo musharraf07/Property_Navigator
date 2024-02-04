6ea3cb4f2668e34dea24c03673a5c3c3cf692c37
Mohammed Musharraf <musharraf.personal@gmail.com>
2024-02-04 18:25:14 +0530

    create get user lisitng api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import Listing from "../models/listing.model.js";
 export const test = (req, res) => {
   res.json({
     message: "API route is working",
@@ -37,7 +38,7 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.id)
+  if (req.user.id === req.params.id)
     return next(errorHandler(401, "You can only delete your own account!"));
   try {
     await User.findByIdAndDelete(req.params.id);
@@ -45,5 +46,18 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    try {
+      const listings = await Listing.find({ userRed: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "you can only view your own listings!"));
   }
 };
